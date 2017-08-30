95537685d1e79f0219b27fb559ff58ca42aa0f82
Arinze39 <arinzenne2017@gmail.com>
2017-08-30 04:27:57 -0700

    Added features

--- app/src/main/java/com/hngintern/arinze/contactcard/MainActivity.java
+++ app/src/main/java/com/hngintern/arinze/contactcard/MainActivity.java
@@ -1,13 +1,19 @@
 package com.hngintern.arinze.contactcard;
 
+import android.media.Image;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView git,slack,mail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +21,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView bio = (TextView) findViewById(R.id.bio);
         bio.setMovementMethod(new ScrollingMovementMethod());
+        mail = (ImageView)findViewById(R.id.mail);
+        git = (ImageView)findViewById(R.id.git);
+        slack = (ImageView)findViewById(R.id.slack);
+        mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMarkerDetails("");
+
+            }
+        });
     }
+
+    public void showMarkerDetails(String Title) {
+        FragmentManager fm = getSupportFragmentManager();
+        EmailFragment emailFragment = EmailFragment.newInstance(Title);
+        emailFragment.show(fm, "fragment_edit_name");
+
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
